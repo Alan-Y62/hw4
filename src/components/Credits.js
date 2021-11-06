ee5610507839dd499ed8ef91517e3b6aa27861f8
Alan Yuan <alan.yuan62@myhunter.cuny.edu>
2021-11-06 19:01:09 -0400

    start credit page

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -3,13 +3,22 @@ import {Link} from 'react-router-dom';
 
 class Credits extends Component {
     render() {
+
+      /* Display all credits */
+      let creditsList = () => {
+      return this.props.credits.map((debit) => {
+          let date = credit.date.slice(0,10);
+          return <li className="list-group-item" key={credit.id}>${credit.amount} | {credit.description} | {date}</li>
+      })
+  }
+
       return (
           <div>
             <div className="navbar">
               <Link className="btn btn-primary mr-2" to="/"> Home </Link>
               <Link className="btn btn-primary mr-2" to="/debits"> Debits </Link>
               <Link className="btn btn-primary mr-2" to="/credits"> Credits </Link>
-              <Link style= {{"margin-right": "300px" }} className="btn btn-primary" to="/userprofile"> User Profile </Link>
+              <Link className="btn btn-primary mr-2" to="/userprofile"> User Profile </Link>
               <Link className="btn btn-primary" to="/login"> Log In </Link>
             </div>
           </div>
